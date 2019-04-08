5b3c0f9b4ee04367adbca051912d3982df8def18
sahil <singla.sahil221@gmail.com>
2019-04-08 19:35:00 +0530

    updted template

--- myapp/serializers/modelsSerializers.py
+++ myapp/serializers/modelsSerializers.py
@@ -17,10 +17,11 @@ class UserQuestionSerializer(serializers.ModelSerializer):
 	question__title  	 = serializers.CharField(source='question.title')
 	question__statement  = serializers.CharField(source='question.statement')
 	question__maximum_marks  = serializers.IntegerField(source='question.maximum_marks')
+	question__id = serializers.CharField(source='question.question_id')
 
 	class Meta:
 		model 			 = UserQuestion
-		fields 			 = ('id', 'level','wrong_attempts', 'marks_obtained', 'question__title', 'question__statement','question__maximum_marks')
+		fields 			 = ('id', 'level','wrong_attempts', 'marks_obtained', 'question__title', 'question__statement','question__maximum_marks','question__id')
 
 
 
