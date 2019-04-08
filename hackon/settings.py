93275f493aee008ff376d7021eea715845cf7fce
Sahil Singla <singlasahil221@gmail.com>
2019-04-08 13:36:20 +0530

    Update settings.py

--- hackon/settings.py
+++ hackon/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ureka_hy%4-p6#ss$+1h25xjti8#u1=a31r=ufeham(%yj(^t4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
